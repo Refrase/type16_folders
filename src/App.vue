b0c1cdf5f6db4e00603b7949234dbe236e2af2be
Andreas Rasmussen <andreasrefras@gmail.com>
2016-12-05 11:35:51 +0100

    Initialized new TYPE16 site with new concept and based on vue.js

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <h1>TYPE16</h1>
+    <p>Site</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+  @import './styles/reset';
+  @import './styles/global';
+  @import './styles/helpers';
+
+  .app {
+    min-height: 100%;
+    width: 100%;
+    background: lighten($color-brandLight, 4%);
+  }
+</style>
